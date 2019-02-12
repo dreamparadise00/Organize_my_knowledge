f2b0171609c70966d83fe2daac844275322d15e5
dreamparadise00 <3110593207@qq.com>
2019-02-12 23:24:04 +0800

    2019.2.12

--- vue-app/vue-app/src/router/index.js
+++ vue-app/vue-app/src/router/index.js
@@ -4,7 +4,16 @@ import HelloWorld from '@/components/HelloWorld'
 import CommonHeader from '@/components/common/CommonHeader'
 import CommonFooter from '@/components/common/CommonFooter'
 import Movie from "@/components/movie/Movie"
+import Music from '@/components/music/Music'
+import Book from '@/components/book/Book'
+import Photo from '@/components/photo/Photo'
 
+import MovieTop250 from '@/components/movie/MovieTop250'
+import Albums from '@/components/music/musicList'
+import Player from '@/components/music/MusicPlayer'
+import PhotoDetail from '@/components/photo/PhotoDetail'
+import Login from '@/components/welcome/Login'
+import Index from '@/components/welcome/Index'
 Vue.use(Router)
 
 export default new Router({
@@ -17,6 +26,15 @@ export default new Router({
     {
       path:'/movie',
       component:Movie
+    },
+    {
+      path     : '/music',
+      component: Music,
+      redirect : '/music/music_albums',
+      children : [
+        {path:'/music/music_albums',component:Albums},
+        {path:'/music/music_player/:id',component:Player}
+      ]
     }
   ]
 })
