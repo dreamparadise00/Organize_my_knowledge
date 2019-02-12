58e0fba461d38b44f32c2c5e1967cd3977bda0d6
dreamparadise00 <3110593207@qq.com>
2019-02-12 12:52:49 +0800

    2019.2.12

---
+++ vue-app/vue-app/src/components/common/CommonHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    <button>首页</button>
+    <h2>movie</h2>
+  </div>
+</template>
+<script>
+</script>
+<style>
+  .header{
+    height: 1rem;
+    background-color: rgb(33,150,243);
+
+  }
+  .header button{
+    width:1rem;
+    height:1rem;
+    color:#fff
+  }
+</style>
