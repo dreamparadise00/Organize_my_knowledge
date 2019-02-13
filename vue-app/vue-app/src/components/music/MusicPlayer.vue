41b550d0107b23d5cfff749420fdf06453e3dcc2
dreamparadise00 <3110593207@qq.com>
2019-02-14 00:05:49 +0800

    2019.2.13

--- vue-app/vue-app/src/components/music/MusicPlayer.vue
+++ vue-app/vue-app/src/components/music/MusicPlayer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class='player'>
-        <a-player :music="musicList" 
+        <a-player :music="musicList"
          v-if="isShow">
         </a-player>
         <!-- {{$route.params.id}} -->
@@ -26,16 +26,16 @@ export default {
       .then((res)=>{
           this.musicData = res.data.musicData;
           console.log(res.data.musicData);
-          for(var i=0;i< this.musicData.length;i++){
-              var obj        = new Object();
-                  obj.title  = this.musicData[i].title;
-                  obj.author = this.musicData[i].author;
-                  obj.url    = this.musicData[i].src;
-                  obj.pic    = this.musicData[i].musicImgSrc;
-                  obj.lrc    = "/static/"+this.musicData[i].lrc;
-                  this.musicList.push(obj);
-          }
-          this.isShow = true;
+          // for(var i=0;i< this.musicData.length;i++){
+          //     var obj        = new Object();
+          //         obj.title  = this.musicData[i].title;
+          //         obj.author = this.musicData[i].author;
+          //         obj.url    = this.musicData[i].src;
+          //         obj.pic    = this.musicData[i].musicImgSrc;
+          //         obj.lrc    = "/static/"+this.musicData[i].lrc;
+          //         this.musicList.push(obj);
+          // }
+          // this.isShow = true;
       });
   },
   components:{
