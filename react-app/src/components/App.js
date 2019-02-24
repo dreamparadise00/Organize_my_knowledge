a2c16686aac8506f2a909d3162d60e794de86083
dreamparadise00 <3110593207@qq.com>
2019-02-24 19:00:25 +0800

    2019.2.24

--- react-app/src/components/App.js
+++ react-app/src/components/App.js
@@ -1,11 +1,17 @@
 import React,{Component} from 'react'
 import { Button } from 'antd-mobile';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home'
 
 export default class App extends Component{
   render(){
     return (
       <div>
-        <Button>button</Button>
+        <Router>
+          <Route path="/" component={Home}></Route>
+          <Route path="/" component={Detail}></Route>
+
+        </Router>
       </div>
     )
   }
