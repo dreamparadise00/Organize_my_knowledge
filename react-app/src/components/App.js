0140946270e38894e4656f05e7ebaf8a61900747
dreamparadise00 <3110593207@qq.com>
2019-02-24 19:25:02 +0800

    2019.2.24

--- react-app/src/components/App.js
+++ react-app/src/components/App.js
@@ -2,14 +2,17 @@ import React,{Component} from 'react'
 import { Button } from 'antd-mobile';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home'
+import Detail from './Detail'
 
 export default class App extends Component{
   render(){
     return (
       <div>
         <Router>
-          <Route path="/" component={Home}></Route>
-          <Route path="/" component={Detail}></Route>
+          <div>
+            <Route path="/" component={Home}></Route>
+            <Route path="/detail" component={Detail}></Route>   
+          </div>
 
         </Router>
       </div>
--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,10 +1,19 @@
-import React,{Component} from 'react'
+import React,{Component} from 'react';
+import { NavBar, Icon } from 'antd-mobile';
 
-export default class Home extends Component{
+export default class Detail extends Component{
   render(){
     return (
       <div>
-        home
+      <NavBar
+      mode="light"
+      icon={<Icon type="left" />}
+      onLeftClick={() => console.log('onLeftClick')}
+      rightContent={[
+        <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+        <Icon key="1" type="ellipsis" />,
+      ]}
+    >首页</NavBar>
       </div>
     )
   }
