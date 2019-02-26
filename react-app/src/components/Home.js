041ace8d85111ba78787775b0c59c08c9b248f19
dreamparadise00 <3110593207@qq.com>
2019-02-26 18:13:26 +0800

    2019.2.24

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -3,12 +3,59 @@ import { NavBar, Icon,SearchBar,Grid} from 'antd-mobile';
 import './../asset/css/home.css';
 import './../asset/css/reset.css';
 
-const data=Array.from(new Array(9)).map((_val,i)) => ({
-  icon:`img${i}`,
-  text:`name${i}`,
-})
+// const data=Array.from(new Array(9)).map((_val,i)) => ({
+//   icon:`img${i}`,
+//   text:`name${i}`,
+// })
 
 export default class Home extends Component{
+  constructor(props) {
+    super(props);
+    this.state ={
+      data:[
+        {
+          icon:require('./../asset/img/meishi.png'),
+          text:"美食"
+        },
+        {
+          icon:require('./../asset/img/dianying.png'),
+          text:"电影"
+        },
+        {
+          icon:require('./../asset/img/jiudian.png'),
+          text:"酒店"
+        },
+        {
+          icon:require('./../asset/img/yule.png'),
+          text:"娱乐"
+        },
+        {
+          icon:require('./../asset/img/waimai.png'),
+          text:"外卖"
+        },
+        {
+          icon:require('./../asset/img/ktv.png'),
+          text:"ktv"
+        },
+        {
+          icon:require('./../asset/img/zhoubianyou.png'),
+          text:"周边游"
+        },
+        {
+          icon:require('./../asset/img/liren.png'),
+          text:"丽人"
+        },
+        {
+          icon:require('./../asset/img/xiaochi.png'),
+          text:"小吃"
+        },
+        {
+          icon:require('./../asset/img/jipiao.png'),
+          text:"机票"
+        }
+      ]
+    }
+  }
   render(){
     return (
       <div>
@@ -21,7 +68,7 @@ export default class Home extends Component{
     >
       <SearchBar placeholder="请输入搜索内容" maxLength={8} />
     </NavBar>
-    <Grid data={data} activeStyle={false} />
+    <Grid data={this.state.data} isCarousel columnNum={5} />
       </div>
     )
   }
