16e4345455fe7f8e2fdfda77b3f6852e9c467601
dreamparadise00 <3110593207@qq.com>
2019-02-26 22:01:02 +0800

    2019.2.26

---
+++ react-app/src/components/Product.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react'
+
+export default() => {
+  return(
+    <div className="pro-container">
+      <img src={require("./../asset/img/yule.png")}></img>
+      <div>
+        <h3 className="pro-title">标题</h3>
+        <p className="pro-detail">这是一个内容</p>
+        <div>
+          <span className="pro-current-price">10</span>
+          <span className="pro-door-price">门市价 20</span>
+          <span className="pro-sold">已售6</span>
+        </div>
+      </div>
+    </div>
+  )
+}
